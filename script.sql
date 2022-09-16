bce412daed0fd82ef6cc20ad359eeb90686909c5
Aline Nunes <72985725+alinenunessouza@users.noreply.github.com>
2022-09-16 19:42:20 -0300

    feat(script): add initial sql script

---
+++ script.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Produto (data_fabricacao DATE
+                      codigo_barras char (30),
+                      custo_unitario DOUBLE,
+                      massa DOUBLE,
+                      volume DOUBLE,
+                      largura DOUBLE,
+                      altura DOUBLE,
+                      comprimento DOUBLE);
