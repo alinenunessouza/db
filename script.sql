d05e988601a0935065a514355fb457be6db778d1
Aline Nunes <72985725+alinenunessouza@users.noreply.github.com>
2022-09-25 20:16:45 -0300

    feat(script): fixing data types and relations

--- script.sql
+++ script.sql
@@ -1,36 +1,37 @@
+-- PostgreSQL 9.6
+
 CREATE TABLE Produto (id uuid default uuid_generate_v4(),
                       fabricacao_timestamp DATE,
-                      custo_unitario DOUBLE,
+                      custo_unitario DECIMAL,
                       nome char (30),
-                      altura DOUBLE.
-                      comprimento DOUBLE.
-                      largura DOUBLE,
-                      massa DOUBLE,
+                      altura DECIMAL,
+                      comprimento DECIMAL,
+                      largura DECIMAL,
+                      massa DECIMAL,
                       codigo_barra char (30),
                       PRIMARY KEY (id)
                       );
                      
-create table Vendido(id uuid default uuid_generate_v4(),
-                      
-                      PRIMARY KEY (id)
-                      );               
+CREATE TABLE Vendido (id uuid default uuid_generate_v4(),
+                     PRIMARY KEY (id)
+                     );      
+                     
+CREATE TABLE Comprador (id uuid default uuid_generate_v4(),
+                        cartao char (16),
+                        PRIMARY KEY (id)
+                        );
  
-                      
+CREATE TABLE Vendedor (id uuid default uuid_generate_v4(),
+                       PRIMARY KEY (id),
+                       registro char (20)
+                      );
+ 
 CREATE TABLE Pedido (id uuid default uuid_generate_v4(),
                      timestamp DATE,
                      PRIMARY KEY (id),
-                     FOREIGN KEY (id_comprador) references Comprador(id),
-                     FOREIGN KEY (id_vendedor) references Vendedor(id)
+                     id_comprador uuid REFERENCES Comprador (id),
+                     id_vendedor uuid REFERENCES Vendedor (id)
                     );
-                    
-                    
-                   
-
-CREATE TABLE Comprador (id uuid default uuid_generate_v4(),
-                       cartao char (16));
- 
-CREATE TABLE Vendedor (id uuid default uuid_generate_v4(),
-                      registro);
 
 CREATE TABLE Usuario (cpf CHAR (11),
                       nome char (10),
