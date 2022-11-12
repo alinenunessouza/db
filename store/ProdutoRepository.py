08dee9d0fd9c9ff200a3b5ee04912a9358d9f16c
Aline Nunes <snunes.aline@gmail.com>
2022-11-11 21:02:05 -0300

    feat(store): updating db class

---
+++ store/ProdutoRepository.py
@@ -0,0 +1,39 @@
+import datetime
+
+class ProdutoRepository:
+
+    def criar(cursor, 
+            conexao,
+            self,
+            id:int=None, 
+            fabricacao_timestamp:datetime=None,
+            custo_unitario:float = None,
+            nome:str = None,
+            altura:float = None,
+            comprimento:float = None,
+            largura:float = None,
+            massa:float = None,
+            codigo_barra:str = None,
+            estoque:int=None):
+        self._id = id
+        self._fabricacao_timestamp = fabricacao_timestamp
+        self._custo_unitario = custo_unitario
+        self._nome = nome
+        self._altura = altura
+        self._comprimento = comprimento
+        self._largura = largura
+        self._massa = massa
+        self._codigo_barra = codigo_barra
+        self._estoque = estoque
+        cursor.execute("INSERT INTO Produto VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+            (id,
+            fabricacao_timestamp,
+            custo_unitario,
+            nome,
+            altura,
+            comprimento,
+            largura,
+            massa,
+            codigo_barra,
+            estoque));
+        conexao.commit()
