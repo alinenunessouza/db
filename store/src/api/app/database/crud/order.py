56bbd9fc18a5d0aa24009b04cf7bbc7fb3b71f5e
Aline Nunes <snunes.aline@gmail.com>
2022-11-15 12:45:58 -0300

    feat(order): add find by id crud

--- store/src/api/app/database/crud/order.py
+++ store/src/api/app/database/crud/order.py
@@ -7,3 +7,10 @@ def find_all(self, cursor):
     for item in cursor.fetchall():
         retorno.append(order(item[0],item[1],item[2],item[3],item[4]))
     return retorno
+
+def find_by_id(self, cursor, id):
+    cursor.execute("SELECT * FROM Pedido WHERE id = {id};")
+    retorno = []
+    for item in cursor.fetchall():
+        retorno.append(order(item[0],item[1],item[2],item[3],item[4]))
+    return retorno
