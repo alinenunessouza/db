227895f75346a9f11ce3c20149c76222ac33225b
Aline Nunes <snunes.aline@gmail.com>
2022-11-15 13:10:44 -0300

    add update by id function

--- store/src/api/app/database/crud/order.py
+++ store/src/api/app/database/crud/order.py
@@ -9,8 +9,12 @@ def find_all(self, cursor):
     return retorno
 
 def find_by_id(self, cursor, id):
-    cursor.execute("SELECT * FROM Pedido WHERE id = {id};")
+    cursor.execute("SELECT * FROM Pedido WHERE id = '{id}';")
     retorno = []
     for item in cursor.fetchall():
         retorno.append(order(item[0],item[1],item[2],item[3],item[4]))
     return retorno
+
+def update_by_id(self, cursor, conexao, id, status):
+    cursor.execute("UPDATE Pedido set status = '{status}' WHERE id = '{id}';")
+    conexao.commit()
