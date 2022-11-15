458a9e43294643e6bbd9523c0f279ef9af474421
Aline Nunes <snunes.aline@gmail.com>
2022-11-15 13:15:48 -0300

    updating crud

--- store/src/api/app/database/crud/user.py
+++ store/src/api/app/database/crud/user.py
@@ -5,5 +5,5 @@ def find_all(self, cursor):
     cursor.execute("SELECT * FROM Usuario;")
     retorno = []
     for item in cursor.fetchall():
-        retorno.append(user(item[0],item[1],item[2],item[3],item[4]))
+        retorno.append(user(item[0], item[1], item[2], item[3], item[4]))
     return retorno
