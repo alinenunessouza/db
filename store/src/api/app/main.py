a112103bcde0641d72aea4a62f088d1f99a25cb2
Davi Schmitz <davi.schmitz@sap.com>
2022-11-18 20:39:52 -0300

    Add products endpoint

--- store/src/api/app/main.py
+++ store/src/api/app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from app.routers import products
+from app.routers import products, users
 
 app = FastAPI()
 
 app.include_router(products.router)
+app.include_router(users.router)
 
 @app.get("/")
 async def root():
--- store/src/api/app/routers/products.py
+++ store/src/api/app/routers/products.py
@@ -2,16 +2,10 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/users/", tags=["users"])
+@router.get("/products", tags=["products"])
 async def read_users():
-    return [{"username": "Rick"}, {"username": "Morty"}]
-
-
-@router.get("/users/me", tags=["users"])
-async def read_user_me():
-    return {"username": "fakecurrentuser"}
-
-
-@router.get("/users/{username}", tags=["users"])
-async def read_user(username: str):
-    return {"username": username}
+    return [
+      {"name": "Celular",
+       "description": "Legal"
+      }
+    ]
