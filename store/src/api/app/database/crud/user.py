6aeee6ff23729011984c94ca2b7fe7cd19aaaee9
Aline Nunes <snunes.aline@gmail.com>
2022-11-22 21:32:41 -0300

    feat: update docs and endpoints

--- store/src/api/app/database/crud/user.py
+++ store/src/api/app/database/crud/user.py
@@ -7,3 +7,24 @@ def find_all(self, cursor):
     for item in cursor.fetchall():
         retorno.append(user(item[0], item[1], item[2], item[3], item[4]))
     return retorno
+
+def create(conexao,
+            self,
+            cpf:str=None, 
+            nome:str=None,
+            sobrenome:str = None,
+            email:str = None,
+            telefone:str = None):
+        self._cpf = cpf
+        self._nome = nome
+        self._sobrenome = sobrenome
+        self._email = email
+        self._telefone = telefone
+        cursor = conexao.cursor()
+        cursor.execute("INSERT INTO Usuario VALUES(%s,%s,%s,%s,%s)",
+            (cpf,
+            nome,
+            sobrenome,
+            email,
+            telefone));
+        conexao.commit()
--- store/src/api/app/routers/users.py
+++ store/src/api/app/routers/users.py
@@ -6,7 +6,6 @@ router = APIRouter()
 async def get_all():
     return [{"username": "Rick"}, {"username": "Morty"}]
 
-
 @router.get("/users/me", tags=["users"])
 async def read_user_me():
     return {"username": "fakecurrentuser"}
@@ -15,3 +14,8 @@ async def read_user_me():
 @router.get("/users/{name}", tags=["users"])
 async def get_by_name(name: str):
     return {"nome": name}
+
+@router.put("/users", tags=["users"], description='Criar um novo perfil de usuário no site.')
+async def create(cpf:str, nome:str, sobrenome:str, email:str, telefone:str):
+    '''userService.Add(cpf, nome, ...)'''
+    return "Cadastro realizado com sucesso!"
