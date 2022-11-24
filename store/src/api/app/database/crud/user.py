10b53e6c907fa001baf82036826f074283808d48
Davi Schmitz <davi.schmitz@sap.com>
2022-11-24 11:12:14 -0300

    Rename variables

--- store/src/api/app/database/crud/user.py
+++ store/src/api/app/database/crud/user.py
@@ -3,10 +3,10 @@ from app.database.models.user import user
 
 def find_all(cursor):
     cursor.execute("SELECT * FROM Usuario")
-    retorno = []
+    result = []
     for item in cursor.fetchall():
-        retorno.append(user(item[0], item[1], item[2], item[3], item[4]))
-    return retorno
+        result.append(user(item[0], item[1], item[2], item[3], item[4]))
+    return result
 
 def create(conexao,
             self,
