0be6a02a51ae0dbc527532fa7889e6cec99947c6
Davi Schmitz <davi.schmitz@sap.com>
2022-11-25 18:57:20 -0300

    Fix order db connection

--- store/src/api/app/database/crud/order.py
+++ store/src/api/app/database/crud/order.py
@@ -2,22 +2,24 @@ from app.database.models.order import Order
 # using time module
 import time
 
-def find_all(cursor):
+def find_all(conn):
+    cursor = conn.cursor()
     cursor.execute("SELECT * FROM Pedido;")
     retorno = []
     for item in cursor.fetchall():
         retorno.append(Order(item[0], item[1], item[2], item[3], item[4]))
     return retorno
 
-def find_by_id(cursor, id):
+def find_by_id(conn, id):
+    cursor = conn.cursor()
     cursor.execute("SELECT * FROM Pedido WHERE id = '" + id + "';")
     for item in cursor.fetchall():
         return Order(item[0], item[1], item[2], item[3], item[4])
     return None
 
-def create(conexao, id, id_products):
+def create(conn, id, id_products):
+    cursor = conn.cursor()
     id_vendedor = "231f342343"
-    cursor = conexao.cursor()
     cursor.execute(
         "INSERT INTO Pedido VALUES(%s,%s,%s,%s)",
         (
@@ -27,12 +29,10 @@ def create(conexao, id, id_products):
         id_vendedor
         ) 
     )
-    conexao.commit()
     return "Pedido realizado com sucesso"
 
-def update_by_id(conexao, id, status):
-    cursor = conexao.cursor()
+def update_by_id(conn, id, status):
+    cursor = conn.cursor()
     cursor.execute(
         "UPDATE Pedido set status = '" + status + "' WHERE id = '" + id + "';"
     )
-    conexao.commit()
--- store/src/api/app/services/orders.py
+++ store/src/api/app/services/orders.py
@@ -1,16 +1,16 @@
 from app.database import dependencies
 from app.database.crud import order
 
-cursor = dependencies.get_connection()
+conn = dependencies.get_connection()
 
 def get_all():
-    return order.find_all(cursor)
+    return order.find_all(conn)
 
 def find_by_id(id:str):
-    return order.find_by_id(cursor, id)
+    return order.find_by_id(conn, id)
 
 def create(cpf:str, id_products:str):
-    return order.create(cursor, cpf, id_products)
+    return order.create(conn, cpf, id_products)
 
 def update_by_id_and_status(id:str, status:str):
-    return order.update_by_id(cursor, id, status)
+    return order.update_by_id(conn, id, status)
