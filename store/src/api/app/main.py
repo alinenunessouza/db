694036a9bd35158d3fe0d7d47f6061befa08a9f4
Aline Nunes <snunes.aline@gmail.com>
2022-11-25 19:31:02 -0300

    feat: temporarily remove cart and order endpoint

--- store/src/api/app/main.py
+++ store/src/api/app/main.py
@@ -3,10 +3,10 @@ from app.routers import cart, products, users, orders
 
 app = FastAPI()
 
-app.include_router(cart.router)
+#app.include_router(cart.router)
 app.include_router(products.router)
 app.include_router(users.router)
-app.include_router(orders.router)
+#app.include_router(orders.router)
 
 @app.get("/")
 async def root():
