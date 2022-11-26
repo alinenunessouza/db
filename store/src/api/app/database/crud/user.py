a0b7e530e0a4a9e985b3d297a1b04bfd06907a9a
Davi Schmitz <davi.schmitz@sap.com>
2022-11-25 21:52:15 -0300

    Formatting

--- store/src/api/app/database/crud/user.py
+++ store/src/api/app/database/crud/user.py
@@ -1,5 +1,6 @@
 from app.database.models.user import User
 
+
 def find_all(conn):
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM Usuario")
@@ -8,12 +9,14 @@ def find_all(conn):
         result.append(User(item[0], item[1], item[2], item[3], item[4]))
     return result
 
+
 def find_by_cpf(conn, cpf: str):
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM Usuario WHERE cpf = %s", (cpf,))
     result = cursor.fetchone()
     return User(result[0], result[1], result[2], result[3], result[4])
-    
+
+
 def create(
     conn,
     cpf: str = None,
@@ -28,15 +31,21 @@ def create(
         (cpf, nome, sobrenome, email, telefone),
     )
 
-def update(conn, 
-    cpf: str, 
+
+def update(
+    conn,
+    cpf: str,
     nome: str = None,
     sobrenome: str = None,
     email: str = None,
-    telefone: str = None
+    telefone: str = None,
 ):
     cursor = conn.cursor()
-    cursor.execute("UPDATE Usuario SET nome = %s, sobrenome = %s, email = %s, telefone = %s WHERE cpf = %s", (nome, sobrenome, email, telefone, cpf))
+    cursor.execute(
+        "UPDATE Usuario SET nome = %s, sobrenome = %s, email = %s, telefone = %s WHERE cpf = %s",
+        (nome, sobrenome, email, telefone, cpf),
+    )
+
 
 def delete_by_cpf(
     conn,
